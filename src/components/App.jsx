29fa22a383d51fd1ff728301a1a611f6c9f5c82e
danadobr3 <134941956+danadobr3@users.noreply.github.com>
2023-11-26 23:45:13 +0100

    datas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,10 @@ import Statistics from './Statistics/Statistics'
 import FriendList from './FriendList/FriendList'
 import TransactionHistory from './TransactionHistory/TransactionHistory'
 
-import user from './data/user.json'
-import data from './data/data.json'
-import friends from './data/friends.json'
-import transactions from './data/transactions.json'
+import user from '../data/user.json'
+import data from '../data/data.json'
+import friends from '../data/friends.json'
+import transactions from '../data/transactions.json'
 
 const App = () => {
   return (
